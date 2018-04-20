9d505d67fa3cf1d142f79dfaf438acb447498807
matheswarwan <mathes.btech@gmail.com>
2018-04-20 16:25:20 +0530

    Update contract-BDA.sol

--- contract-BDA.sol
+++ contract-BDA.sol
@@ -12,8 +12,11 @@ contract BDA {
         string forestAuthComments;
     }
     
+    
     mapping (bytes32 => Land) public land;
     address public contractOwner = msg.sender;
+    mapping (address => bool) public lakeApprover;
+    mapping (address => bool) public forestApprover;
 
     modifier isOpenForApproval (uint8 _approverType, bytes32 _id) {
         if(_approverType == 0) {
@@ -46,11 +49,26 @@ contract BDA {
         contractOwner = _newOwnerAddress;
     }
     
+    /* function addLakeApprover(address _lakeApprover)  public onlyOwner {
+        lakeApprover[_lakeApprover] = true;
+    }
+    
+    function addForestApprover(address _forestApprover)  public onlyOwner {
+        forestApprover[_forestApprover] = true;
+    }*/
+    
     function viewLand(bytes32 _id) public constant returns (string  _projLocation, uint256 _surveyNumber, uint8 _nocStatus, uint8 _lakeApproverStatus, string _lakeComments, uint8 _forestApproverStatus, string _forestComments)  {
         return (land[_id].projLocation, land[_id].surveyNumber, land[_id].nocStatus, land[_id].lakeAuthApproverStatus, land[_id].lakeAuthComments, land[_id].forestAuthApproverStatus, land[_id].forestAuthComments);
     }
     
-
+    /*function isValidLakeApprover(address _lakeApprover) returns (bool){
+        return lakeApprover[_lakeApprover];
+    }
+    
+	function isValidForestApprover(address _forestApprover) returns (bool){
+        return forestApprover[_forestApprover];
+    }*/
+    
    function registerLand (string  _location, string  _name, bytes32 _id, uint256 _surveyNumber) public onlyOwner payable returns (bool) { 
         if(!isLandAlreadyRegistered(_id)) {
             land[_id].projLocation = _location;
@@ -98,14 +116,20 @@ contract BDA {
     }
     
     function approveNOC (uint8 _approverType, bytes32 _id, uint8 _approvalStatus, string _approverComments) public payable isOpenForApproval(_approverType,_id) returns (bool) {
-        if(_approverType == 0) {
-            //Lake
-            land[_id].lakeAuthApproverStatus = _approvalStatus;
-            land[_id].lakeAuthComments = _approverComments;
-        }else if (_approverType == 1) {
-            //Forest
-            land[_id].forestAuthApproverStatus = _approvalStatus;
-            land[_id].forestAuthComments = _approverComments;
+        if(_approverType == 0) { //Lake
+            //Ensure that this is a valid lake approver
+            //if(isValidLakeApprover(msg.sender)) 
+            {
+                land[_id].lakeAuthApproverStatus = _approvalStatus;
+                land[_id].lakeAuthComments = _approverComments;
+            }
+        }else if (_approverType == 1) {  //Forest
+            //Ensure that this is a valid Forest approver
+            //if(isValidForestApprover(msg.sender)) 
+            {
+                land[_id].forestAuthApproverStatus = _approvalStatus;
+                land[_id].forestAuthComments = _approverComments;
+            }
         }
         
         //Check if Overall NOC status can be updated
